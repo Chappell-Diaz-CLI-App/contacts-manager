57fc8a651ca764840139d32419ef56553c12b07a
Nicholas <nicholasdiazjr509@gmail.com>
2022-01-14 12:16:46 -0600

    created Test file

--- src/Contact.java
+++ src/Contact.java
@@ -1,22 +1,38 @@
-public class Contact{
+public class Contact {
     private String fullName;
-    private String phoneNumber;
+    private String phoneNumber;//double or string??
 
-    public String getFullName(){
-        return fullName;
-}
-    public void setFullName(String fullName){
-        this.fullName = fullName;
-    }
-    public String getPhoneNumber(){
-        return phoneNumber = phoneNumber;
-    }
-    Contact(){
-        fullName = null;
-        phoneNumber = null;
-    }
-    public Contact(String name, String phoneNumber){
+    public Contact(String name, String phoneNumber) {
         this.fullName = name;
         this.phoneNumber = phoneNumber;
     }
-}
+
+    public Contact(String fileEntry) {
+        String[] data = fileEntry.split(" | ");
+        this.fullName = data[0];
+        this.phoneNumber = data[data.length - 1];
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+        public void setPhoneNumber(String phoneNumber){
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String toString() {
+            return "Name: " + fullName + ", Number: " + phoneNumber;
+        }
+        public String toFileString () {
+            return fullName + " | " + phoneNumber;
+        }
+    }
+
