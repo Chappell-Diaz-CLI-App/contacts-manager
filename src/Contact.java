8d326177c99314f7327532642bccf559373a693a
Nicholas <nicholasdiazjr509@gmail.com>
2022-01-14 11:36:50 -0600

    finished contact file draft

--- src/Contact.java
+++ src/Contact.java
@@ -1,4 +1,22 @@
 public class Contact{
+    private String fullName;
+    private String phoneNumber;
 
-
+    public String getFullName(){
+        return fullName;
+}
+    public void setFullName(String fullName){
+        this.fullName = fullName;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber = phoneNumber;
+    }
+    Contact(){
+        fullName = null;
+        phoneNumber = null;
+    }
+    public Contact(String name, String phoneNumber){
+        this.fullName = name;
+        this.phoneNumber = phoneNumber;
+    }
 }
